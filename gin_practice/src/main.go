5be76547af7be0b57f9a127599251a2d5817c8d8
BinDruid <abharya.dev@gmail.com>
2024-07-05 17:25:56 +0330

    [Refactor] Add albums package

--- gin_practice/src/main.go
+++ gin_practice/src/main.go
@@ -1,64 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/BinDruid/go-practice/gin_practice/src/albums"
 	"github.com/gin-gonic/gin"
 )
-
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func createAlbum(c *gin.Context) {
-	var newAlbum album
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
 
 func main() {
 	router := gin.Default()
 	router.Static("/static/", "../public")
 	albumsRouter := router.Group("/albums")
-	albumsRouter.GET("/", getAlbums)
-	albumsRouter.GET("/:id", getAlbumByID)
-	albumsRouter.POST("/", createAlbum)
-	// Health check route
+	albumsRouter.GET("/", albums.GetAll)
+	albumsRouter.GET("/:id", albums.GetByID)
+	albumsRouter.POST("/", albums.Create)
 	router.GET("/healthcheck", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
 		})
 	})
 
-	router.Run(":4030")
+	err := router.Run(":4030")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
