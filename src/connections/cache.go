461cef144dcae4cd385362ae41d80262255ee9d8
BinDruid <abharya.dev@gmail.com>
2024-07-11 00:08:37 +0330

    [Refactor] Recognize folders to fit all in src

--- src/connections/cache.go
+++ src/connections/cache.go
@@ -1,14 +1,19 @@
 package connections
 
 import (
+	"fmt"
 	"github.com/redis/go-redis/v9"
+	"os"
 )
 
 var Redis *redis.Client
 
 func InitCache() {
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6378",
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
 		Password: "",
 		DB:       0,
 	})
--- src/connections/db.go
+++ src/connections/db.go
@@ -1,15 +1,22 @@
 package connections
 
 import (
+	"fmt"
 	"github.com/BinDruid/go-practice/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var Postgres *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=go_practice password=123456 dbname=go_practice port=5314 sslmode=disable TimeZone=Asia/Tehran"
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tehran", dbHost, dbUser, dbPass, dbName, dbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
