80715a03f970e05474e12153adf5f46ca34b4488
Ali Abharya <99214334+BinDruid@users.noreply.github.com>
2024-07-11 15:57:09 +0330

    [Feat] Enable routes and middleware (#4)
    * [Chore] Add request id middleware
    
    * [Chore] Add album routes
    
    * [Feat] Enable routes and middleware

--- src/main.go
+++ src/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"github.com/BinDruid/go-practice/connections"
-	"github.com/BinDruid/go-practice/cruds"
+	"github.com/BinDruid/go-practice/middlewares"
+	"github.com/BinDruid/go-practice/routes"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -15,13 +16,11 @@ func init() {
 
 func main() {
 	router := gin.Default()
+	router.Use(middlewares.RequestIDMiddleware())
 	router.Static("/static/", "../public/")
 	api := router.Group("/api")
 	v1 := api.Group("/v1")
-	albumsRouter := v1.Group("/albums/")
-	albumsRouter.GET("/", cruds.GetAllAlbums)
-	albumsRouter.GET("/:id/", cruds.GetAlbumByID)
-	albumsRouter.POST("/", cruds.CreateAlbum)
+	routes.AddAlbumsRoute(v1)
 	router.GET("/healthcheck/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
