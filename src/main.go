310d6d3686e2052330810c839b1d672edbb98ac4
BinDruid <abharya.dev@gmail.com>
2024-07-11 19:17:15 +0330

    [Feat] Add swagger documentation

--- src/main.go
+++ src/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/BinDruid/spotify-gin/db"
+	docs "github.com/BinDruid/spotify-gin/docs"
 	"github.com/BinDruid/spotify-gin/middlewares"
 	"github.com/BinDruid/spotify-gin/routes"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 	"net/http"
 )
@@ -14,6 +17,12 @@ func init() {
 	db.InitRedis()
 }
 
+//	@title			Minimal Spotify API
+//	@version		1.0
+//	@description	Yet Another clone of spotify api built with gin.
+
+// @host		localhost:8000
+// @BasePath	/api/v1
 func main() {
 	router := gin.Default()
 	router.Use(middlewares.RequestIDMiddleware())
@@ -21,6 +30,8 @@ func main() {
 	api := router.Group("/api")
 	v1 := api.Group("/v1")
 	routes.AddAlbumsRoute(v1, "/albums")
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	router.GET("/healthcheck/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK",
