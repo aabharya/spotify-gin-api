3406327ee31ea65a618a517b12900d232a509597
BinDruid <abharya.dev@gmail.com>
2024-07-11 16:26:24 +0330

    [Refactor] Use db settings to connect

--- src/connections/cache.go
+++ src/connections/cache.go
@@ -1,20 +1,17 @@
 package connections
 
 import (
-	"fmt"
+	"github.com/BinDruid/go-practice/settings"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/redis/go-redis/v9"
-	"os"
 )
 
 var Redis *redis.Client
 
 func InitCache() {
-	redisHost := os.Getenv("REDIS_HOST")
-	redisPort := os.Getenv("REDIS_PORT")
 
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
+		Addr:     settings.Configs.RedisUrl,
 		Password: "",
 		DB:       0,
 	})
